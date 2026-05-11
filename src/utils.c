ffad8223a0746640c467532be99b29878ace124a
j-miet <191247923+j-miet@users.noreply.github.com>
2026-05-11 13:45:59 +0300

    Add simple logging helpers

---
+++ src/utils.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+#include "utils.h"
+
+void log_info(const char* msg) {
+    printf("[INFO] %s\n", msg);
+}
+
+void log_error(const char* msg) {
+    printf("[ERROR] %s\n", msg);
+}
