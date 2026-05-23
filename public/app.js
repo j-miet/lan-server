b32da405e22489374a08fbd06149bce312ea738d
j-miet <191247923+j-miet@users.noreply.github.com>
2026-05-23 20:36:42 +0300

    Update public web UI

---
+++ public/app.js
@@ -0,0 +1,72 @@
+const dropZone = document.getElementById("drop-zone");
+const fileInput = document.getElementById("file-input");
+const fileList = document.getElementById("file-list");
+
+// file drag & drop handlers: these call hidden input element
+dropZone.addEventListener("click", () => fileInput.click());
+
+dropZone.addEventListener("dragover", (e) => {
+  e.preventDefault();
+  dropZone.classList.add("dragover");
+});
+
+dropZone.addEventListener("dragleave", () =>
+  dropZone.classList.remove("dragover"),
+);
+
+dropZone.addEventListener("drop", (e) => {
+  e.preventDefault();
+  dropZone.classList.remove("dragover");
+
+  const files = e.dataTransfer.files;
+
+  uploadFiles(files);
+});
+
+fileInput.addEventListener("change", () => uploadFiles(fileInput.files));
+
+/**
+ * Get all uploaded server files and create WebUI entries with download links for each
+ */
+async function loadFiles() {
+  const response = await fetch("/api/files");
+
+  const files = await response.json();
+
+  fileList.innerHTML = "";
+
+  for (const file of files) {
+    const div = document.createElement("div");
+
+    div.className = "file-entry";
+    div.innerHTML = `<a href="/download/${encodeURIComponent(file)}">
+            ${file}
+        </a>`;
+
+    fileList.appendChild(div);
+  }
+}
+
+/**
+ * uploading to server
+ */
+async function uploadFiles(files) {
+  for (const file of files) {
+    const formData = new FormData();
+
+    formData.append("file", file);
+
+    const response = await fetch("/api/upload", {
+      method: "POST",
+      body: formData,
+    });
+
+    if (!response.ok) {
+      alert("Upload failed: " + file.name);
+    }
+  }
+
+  loadFiles();
+}
+
+loadFiles();
