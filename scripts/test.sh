0957816dfa09c98e901dfc22d2be730233e83718
j-miet <191247923+j-miet@users.noreply.github.com>
2026-05-31 15:29:28 +0300

    Minor change

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo "this script will count from 1 to 10 and then prints user input and selected message"
-for i in {1..10}; do
+echo "this script will count from 1 to 5, then prints input and selected message"
+for i in {1..5}; do
     echo "=> $i"
     sleep 1
 done
