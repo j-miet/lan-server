d9b6d31d35d3d4673fd2f081e0564c7fa73bfefb
j-miet <191247923+j-miet@users.noreply.github.com>
2026-06-04 14:00:36 +0300

    Lower counter to 3 seconds

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo "this script will count from 1 to 5, then prints input and selected message"
-for i in {1..5}; do
+echo "this script will count from 1 to 3, then prints input and selected message"
+for i in {1..3}; do
     echo "=> $i"
     sleep 1
 done
